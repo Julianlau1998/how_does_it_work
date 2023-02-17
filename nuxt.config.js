4d4ef43fa1dc7111e52a3233c9348ea672b22015
julianlau1998 <Julian.lau@aol.com>
2023-02-17 15:21:25 +0100

    font loading

--- nuxt.config.js
+++ nuxt.config.js
@@ -120,6 +120,12 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
+    defaultAssets: {
+      font: {
+        family: 'Roboto'
+      }
+    },
     theme: {
       dark: true,
       themes: {
