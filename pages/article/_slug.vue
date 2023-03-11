d1b649d180ef8c8f6daf2901c9a4ca975250b89d
julianlau1998 <Julian.lau@aol.com>
2023-03-11 23:05:11 +0100

    test remove fetch on articles

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -132,17 +132,16 @@ export default {
       ]
     }
   },
-  async fetch () {
-      await this.$store.dispatch('getArticles')
-      await this.$store.dispatch('getCategories')
-  },
   data () {
     return {
       rating: 0,
       shareAvailable: false
     }
   },
-  mounted () {
+  async mounted () {
+    await this.$store.dispatch('getArticles')
+    await this.$store.dispatch('getCategories')
+
     this.shareAvailable = navigator.share !== undefined
   },
   computed: {
